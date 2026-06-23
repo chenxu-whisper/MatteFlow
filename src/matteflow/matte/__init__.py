d9d3cdef351c36f17d743cf0161e9b453942d95c
chenxu-whisper <1070669767@qq.com>
2026-06-23 19:37:53 +0800

    feat: add quality gated matte fusion

--- src/matteflow/matte/__init__.py
+++ src/matteflow/matte/__init__.py
@@ -1,5 +1,13 @@
-from .green_screen_matte import GreenScreenMatte
 from .black_background_matte import BlackBackgroundMatte
+from .fusion_quality_gate import FusionCandidate, FusionQualityGate, FusionResult
+from .green_screen_matte import GreenScreenMatte
 from .matte_fusion import MatteFusion
 
-__all__ = ["GreenScreenMatte", "BlackBackgroundMatte", "MatteFusion"]
+__all__ = [
+    "BlackBackgroundMatte",
+    "FusionCandidate",
+    "FusionQualityGate",
+    "FusionResult",
+    "GreenScreenMatte",
+    "MatteFusion",
+]
